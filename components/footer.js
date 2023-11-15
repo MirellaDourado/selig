bbacb4102962b520c5e8562ff64d5864d8447dac
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-11-15 10:22:10 -0300

    hedader and footer

---
+++ components/footer.js
@@ -0,0 +1,38 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <footer class="seligFooter">
+      <div>
+        <a> A SeLiG </a>
+        <a> Assistência técnica autorizada para produtos fora de garantia </a>
+        <a> Assistência técnica autorizada para produtos fora de garantia </a>
+        <a> Assistência técnica especializada </a>
+      </div>
+      <div>
+        <img src="/assets/selig-logo.png" alt='palavra SeLiG escrita na cor laranja' />
+        <div>
+          <a target="_blank" rel='noopener noreferrer' href='https://goo.gl/maps/XBSgCpbbJbU3ymFV6'> <span> Endereço: </span> Rua Bahia, 176 - Pituba, Salvador - BA, 41830-160</a>
+          <a target="_blank" rel='noopener noreferrer' href='https://api.whatsapp.com/send/?phone=557122037800&text&type=phone_number&app_absent=0'> <span> Telefone: </span> (71) 2203-7800 </a>
+        </div>
+        <div>
+          <a target="_blank" rel='noopener noreferrer' href='https://www.instagram.com/assistenciaselig/'>
+            INSTAGRAM
+          </a>
+          <Link>
+            TIKTOK
+          </Link>
+          <Link>
+            LINKEDIN
+          </Link>
+        </div>
+      </div>
+      <p style={{ fontSize: '0.8em' }}> Copyright © 2006-2023 SELIG. Todos os direitos reservados. </p>
+    </footer>`
+  }
+}
+
+customElements.define('footer-component', Footer);
---
+++ components/header.js
@@ -0,0 +1,42 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `<section class="classMom">
+    <section class="headerSection">
+      <div>
+        <a target="_blank" rel='noopener noreferrer'href='https://api.whatsapp.com/send/?phone=557122037800&text&type=phone_number&app_absent=0'> FALE CONOSCO </a>
+          <a target="_blank" rel='noopener noreferrer'href='https://www.instagram.com/assistenciaselig/'> INSTAGRAM </a>
+          <a target="_blank" rel='noopener noreferrer'href='https://www.tiktok.com/assistenciaselig/'>  TIKTOK </a>
+          <a target="_blank" rel='noopener noreferrer'href='https://www.linkedin.com/company/assistencia-selig/'> LINKEDIN </a>
+      </div>
+    </section>
+    <header class="seligHeader">
+      <div>
+        <a href="/" class="logo"> <img src="/assets/selig-logo.png" alt="nome SeLiG em laranja"></a>
+        <nav id="nav">
+          <button class="btnMobile"> MENU </button>
+          <ul id="menu">
+            <li> <a href="/selig"> A SeLiG </a></li>
+            <li>
+              <a href="/#services"> Nossos Serviços </a>
+              <div class="hidenMenu">
+                <a href="/dentro-de-garantia">Assistência para produtos dentro de garantia</a>
+                <a href="/fora-de-garantia">Assistência para produtos fora de garantia</a>
+                <a href="/especializada">Assistência técnica especializada</a>
+              </div>
+            </li>
+            <li>
+              <a href="/#contact"> Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </section>`
+  }
+}
+
+customElements.define('header-component', Header);
